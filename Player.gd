e2205c4c8d644183b5049f53154cda7f96672924
CRBroughton <crbroughton@posteo.uk>
2022-04-02 08:47:38 +0100

    hidden player sprite

--- Player.gd
+++ Player.gd
@@ -7,6 +7,7 @@ var screen_size:Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 	pass # Replace with function body.
 
 
