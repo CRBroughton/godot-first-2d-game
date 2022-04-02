57fce19af6b20f20e7e59e35d6bc727c180d0b8f
CRBroughton <crbroughton@posteo.uk>
2022-04-02 09:26:53 +0100

    added game_over and new_game funcs

--- Main.gd
+++ Main.gd
@@ -4,4 +4,13 @@ export(PackedScene) var mob_scene # allows us to choose Mob scene to instance
 var score
 
 func _ready():
-    randomize()
+	randomize()
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
